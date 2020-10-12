ffd799734484e1ec7af52df9f590cd604a40f0cb
Rutmank <rutmank@yandex.ru>
2020-10-12 12:28:51 +0300

    Добавьте файлы проекта.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec;
+            int min = 0;
+            int hour = 0;
+
+            for (sec = 1; ; sec++)
+            {
+                if (sec % 60 == 0)
+                {
+                    sec = 0;
+                    min++;
+                    Console.WriteLine("Время: " + hour + ":" + min + ":" + sec);
+                    System.Threading.Thread.Sleep(1000);
+
+                    if (min % 60 == 0)
+                    {
+                        min = 0;
+                        hour++;
+                        Console.WriteLine("Время: " + hour + ":" + min + ":" + sec);
+                        System.Threading.Thread.Sleep(1000);
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Время: " + hour + ":" + min + ":" + sec);
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stopwatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Stopwatch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stopwatch")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("52ed421e-899b-48ed-a39b-45700753b9de")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
